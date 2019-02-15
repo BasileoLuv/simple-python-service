102e1b343351351f3f476a5029cc98c04383cae1
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-02-15 15:54:46 +0300

    Update version.py

--- version.py
+++ version.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import os, sys
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 dt = datetime.strftime(datetime.now(), "%Y%m%d.%H%M%S")
-newname = 'dist/Python-Calculator-0.4-'+dt+'.tar.gz'
-os.rename('./dist/Python-Calculator-0.4-SNAPSHOT.tar.gz', newname)
-newname2 = 'dist/Python-Calculator-0.4-'+dt+'.whl'
-os.rename('./dist/Python_Calculator-0.4_SNAPSHOT-py2-none-any.whl', newname2)
+newname = 'dist/Python-Calculator-0.4.1-'+dt+'.tar.gz'
+os.rename('./dist/Python-Calculator-0.4.1-SNAPSHOT.tar.gz', newname)
+newname2 = 'dist/Python-Calculator-0.4.1-'+dt+'.whl'
+os.rename('./dist/Python_Calculator-0.4.1_SNAPSHOT-py2-none-any.whl', newname2)
