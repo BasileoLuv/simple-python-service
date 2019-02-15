fde36b44d624642e50f15165c8ecad9e7cabcb8b
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-02-15 10:28:57 +0300

    Update artifactversion.sh

--- artifactversion.sh
+++ artifactversion.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-function extract_version_without_snapshot(){
-   grep version $1 | cut -d= -f2 | cut -d- -f1 
-
+function myfunc {
+local temp=$[ $value + 5 ]
+echo "The Temp from inside function is $temp"
 }
-
-function bump_hotfix_version()
-{
-   major_version=$(echo $1 | cut -d. -f1)
-   minor_version=$(echo $1 | cut -d. -f2)
-   hotfix_version=$(echo $1 | cut -d. -f3)
-   echo "${major_version}.${minor_version}.$(( ${hotfix_version}+1))"
-}
-
-function generate_bumped_setup()
-{
-   current_version=$(extract_version_without_snapshot setup.py)
-   new_version=$(bump_hotfix_version $current_version)
-   sed "s/version=.*/version=$new_version-SNAPSHOT',/g" setup.py >1.txt
-}
+temp=4
+myfunc
+echo "The temp from outside is $temp"
