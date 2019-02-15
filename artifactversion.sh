32128ce63d1dfc1579710df803ae52a9142845a3
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-02-15 10:22:50 +0300

    Update artifactversion.sh

--- artifactversion.sh
+++ artifactversion.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-function myfunc {
-echo "The first function definition"
+function extract_version_without_snapshot(){
+   grep version $1 | cut -d= -f2 | cut -d- -f1 
+
 }
-myfunc
-function myfunc {
-echo "The second function definition"
+
+function bump_hotfix_version()
+{
+   major_version=$(echo $1 | cut -d. -f1)
+   minor_version=$(echo $1 | cut -d. -f2)
+   hotfix_version=$(echo $1 | cut -d. -f3)
+   echo "${major_version}.${minor_version}.$(( ${hotfix_version}+1))"
 }
-myfunc
-echo "End of the script"
+
+function generate_bumped_setup()
+{
+   current_version=$(extract_version_without_snapshot setup.py)
+   new_version=$(bump_hotfix_version $current_version)
+   sed "s/version=.*/version=$new_version-SNAPSHOT',/g" setup.py >1.txt
+   mv 1.txt setup.py
+}
