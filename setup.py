061632a577567ea66897c8cf2ca6048e2a2f6501
BasileoLuv <idronandrew@gmail.com>
2019-02-25 12:19:03 +0000

    updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class Install(InstallCommand):
 
 setup(
     name="Python-Calculator",
-    version='0.4.5-SNAPSHOT',
+    version='0.4.6-SNAPSHOT',
     packages=['calc'],
     test_suite="tests",
     scripts=[
