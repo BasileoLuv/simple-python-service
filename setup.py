5020335a995c137c7ae63f19534cb68cec4bd93f
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-02-26 10:10:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class Install(InstallCommand):
 
 setup(
     name="Python-Calculator",
-    version='0.4.8-SNAPSHOT',
+    version='0.4.9-SNAPSHOT',
     packages=['calc'],
     test_suite="tests",
     scripts=[
