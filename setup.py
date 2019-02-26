24af4d65068370ca1769f014c67b73a50681d8ab
BasileoLuv <idronandrew@gmail.com>
2019-02-26 11:32:37 +0000

    updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class Install(InstallCommand):
 
 setup(
     name="Python-Calculator",
-    version='0.4.12',
+    version='0.4.13-SNAPSHOT',
     packages=['calc'],
     test_suite="tests",
     scripts=[
